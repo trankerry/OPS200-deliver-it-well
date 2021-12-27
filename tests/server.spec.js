e498bee95f79a182b5b87479edc0460c709fca9b
Kerry <trankerry94@gmail.com>
2021-12-27 13:55:48 -0800

    finished

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,15 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page says hello world', (done) => {
+		chai
+			.request(server)
+			.get('/')
+			.end((err, res) => {
+				expect(err).not.exist;
+				expect(JSON.stringify(res.text)).to.contain('Banana');
+				done();
+			});
+	});
 })
